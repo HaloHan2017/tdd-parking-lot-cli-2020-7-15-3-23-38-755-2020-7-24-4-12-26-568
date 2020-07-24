2c9799491ecf6783070cafb2e864e488a4e09a08
HaloHan2017 <2332722908@qq.com>
2020-07-24 16:05:29 +0800

    [ParkingCar] [jerry] feature :implement should return null when fetch given used car ticket

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,10 +1,13 @@
 package com.oocl.cultivation;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class ParkingLot {
     private final Map<CarTicket, Car> parkingRoom = new HashMap<>();
+    private final Set<CarTicket> carTicketRecords = new HashSet<>();
 
     public CarTicket park(Car car) {
         CarTicket carTicket = new CarTicket();
@@ -13,6 +16,10 @@ public class ParkingLot {
     }
 
     public Car fetch(CarTicket ticket) {
+        if(carTicketRecords.contains(ticket)){
+            return null;
+        }
+        carTicketRecords.add(ticket);
         return parkingRoom.get(ticket);
     }
 }
