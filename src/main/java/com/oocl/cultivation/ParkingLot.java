98e3610dd38c98a8237f4d4b0c1e639940180cb5
HaloHan2017 <2332722908@qq.com>
2020-07-24 15:46:45 +0800

    [ParkingCar] [jerry] feature :implement fetch method in ParkingLot

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,11 +1,18 @@
 package com.oocl.cultivation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
+    private final Map<CarTicket, Car> parkingRoom = new HashMap<>();
+
     public CarTicket park(Car car) {
-        return new CarTicket();
+        CarTicket carTicket = new CarTicket();
+        parkingRoom.put(carTicket, car);
+        return carTicket;
     }
 
     public Car fetch(CarTicket ticket) {
-        return null;
+        return parkingRoom.get(ticket);
     }
 }
