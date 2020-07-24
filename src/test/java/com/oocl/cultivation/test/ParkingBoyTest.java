36673254d6688646ebea320971d7ab8f71fcf219
HaloHan2017 <1006653740@qq.com>
2020-07-24 20:39:18 +0800

    [ParkingCar] [jerry] test: add should_return_fetch_car_when_fetch_given_car_ticket in ParkingBoyTest and create fetch method in ParkingB oy

--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -3,8 +3,10 @@ package com.oocl.cultivation.test;
 import com.oocl.cultivation.Car;
 import com.oocl.cultivation.CarTicket;
 import com.oocl.cultivation.ParkingBoy;
+import com.oocl.cultivation.ParkingLot;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingBoyTest {
@@ -17,5 +19,18 @@ public class ParkingBoyTest {
         CarTicket ticket = parkingBoy.park(car);
         // then
         assertNotNull(ticket);
+    }
+
+    @Test
+    void should_return_fetch_car_when_fetch_given_car_ticket() {
+        // given
+        Car car = new Car();
+        ParkingBoy parkingBoy = new ParkingBoy();
+        CarTicket ticket = parkingBoy.park(car);
+        // when
+        Car fetchedCar = parkingBoy.fetch(ticket);
+        // then
+        assertNotNull(fetchedCar);
+        assertEquals(car, fetchedCar);
     }
 }
