dd3cdfe589cbd70bd5cf461b57f15a36d4ec9f0b
HaloHan2017 <2332722908@qq.com>
2020-07-24 17:38:28 +0800

    [ParkingCar] [jerry] feature: add should_return_car_ticket_when_park_given_car in ParkingBoyTest and create park method for ParkingBoy

---
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -0,0 +1,13 @@
+package com.oocl.cultivation;
+
+public class ParkingBoy {
+    private ParkingLot parkingLot;
+
+    public ParkingBoy() {
+        this.parkingLot = new ParkingLot();
+    }
+
+    public CarTicket park(Car car) {
+        return null;
+    }
+}
