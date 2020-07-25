e0eda7ea4b4652129351846e9248304df74ca9ff
HaloHan2017 <1006653740@qq.com>
2020-07-25 17:35:13 +0800

    [ParkingCar] [jerry] feature : implement park method in SmartParkingBoy and should return car ticket when park smartly given car in ParkingBoyTest

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -1,12 +1,20 @@
 package com.oocl.cultivation;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class SmartParkingBoy {
+public class SmartParkingBoy extends ParkingBoy {
+    private List<ParkingLot> parkingLots;
+
     public SmartParkingBoy(List<ParkingLot> parkingLots) {
+        this.parkingLots = parkingLots;
     }
 
-    public CarTicket park(Car car) {
-        return null;
+    @Override
+    public CarTicket park(Car car) throws CustomException {
+        List<Integer> parkingRoomRemindCapacities = parkingLots.stream().map(ParkingLot::getParkingRoomRemindCapacity).collect(Collectors.toList());
+        int theMaxRemindCapacity = parkingRoomRemindCapacities.stream().reduce(Integer::max).get();
+        int theIndexOfTheMaxRemindCapacity = parkingRoomRemindCapacities.indexOf(theMaxRemindCapacity);
+        return parkingLots.get(theIndexOfTheMaxRemindCapacity).park(car);
     }
 }
