847f29a3f77cc5ed0c40f068a05313bd2f29226f
HaloHan2017 <1006653740@qq.com>
2020-07-27 19:16:04 +0800

    [ParkingCar] [jerry] refactor : move should_return_car_ticket_when_park_super_smartly_given_car from ParkingBoyTest to SuperSmartParkingBoyTest

--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -89,25 +89,4 @@ public class ParkingBoyTest {
         // then
         assertEquals("Not enough position.", errorMsg);
     }
-
-    @Test
-    void should_return_car_ticket_when_park_super_smartly_given_car() throws CustomException {
-        // given
-        Car car = new Car();
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        for (int i = 0; i < 2; i++) {
-            parkingLots.add(new ParkingLot(i + 5));
-        }
-        for (int i = 0; i < 3; i++) {
-            parkingLots.get(0).park(new Car());
-            parkingLots.get(1).park(new Car());
-        }
-        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
-        // when
-        CarTicket ticket = superSmartParkingBoy.park(car);
-        // then
-        assertNotNull(ticket);
-        assertEquals(2, parkingLots.get(0).getParkingRoomRemindCapacity());
-        assertEquals(2, parkingLots.get(1).getParkingRoomRemindCapacity());
-    }
 }
---
+++ src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
@@ -0,0 +1,34 @@
+package com.oocl.cultivation.test;
+
+import com.oocl.cultivation.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SuperSmartParkingBoyTest {
+
+    @Test
+    void should_return_car_ticket_when_park_super_smartly_given_car() throws CustomException {
+        // given
+        Car car = new Car();
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            parkingLots.add(new ParkingLot(i + 5));
+        }
+        for (int i = 0; i < 3; i++) {
+            parkingLots.get(0).park(new Car());
+            parkingLots.get(1).park(new Car());
+        }
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
+        // when
+        CarTicket ticket = superSmartParkingBoy.park(car);
+        // then
+        assertNotNull(ticket);
+        assertEquals(2, parkingLots.get(0).getParkingRoomRemindCapacity());
+        assertEquals(2, parkingLots.get(1).getParkingRoomRemindCapacity());
+    }
+}
