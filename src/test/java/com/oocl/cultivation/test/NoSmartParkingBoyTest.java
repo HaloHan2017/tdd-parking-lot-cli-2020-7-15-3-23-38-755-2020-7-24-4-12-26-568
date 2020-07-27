a1eeae0d7ffc2e2c9ab044a456c7d57a731f5e71
HaloHan2017 <2332722908@qq.com>
2020-07-27 17:49:37 +0800

    [ParkingCar] [jerry] refactor : move should_return_car_ticket_when_park_sequentially_given_car from ParkingBoyTest to NoSamrtParkingBoyTest

---
+++ src/test/java/com/oocl/cultivation/test/NoSmartParkingBoyTest.java
@@ -0,0 +1,30 @@
+package com.oocl.cultivation.test;
+
+import com.oocl.cultivation.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class NoSmartParkingBoyTest {
+
+    @Test
+    void should_return_car_ticket_when_park_sequentially_given_car() throws CustomException {
+        // given
+        Car car = new Car();
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            parkingLots.add(new ParkingLot());
+        }
+        NoSmartParkingBoy noSmartParkingBoy = new NoSmartParkingBoy(parkingLots);
+        // when
+        CarTicket ticket = noSmartParkingBoy.park(car);
+        // then
+        assertNotNull(ticket);
+        assertEquals(9, parkingLots.get(0).getParkingRoomRemindCapacity());
+        assertEquals(10, parkingLots.get(1).getParkingRoomRemindCapacity());
+    }
+}
--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -91,23 +91,6 @@ public class ParkingBoyTest {
     }
 
     @Test
-    void should_return_car_ticket_when_park_sequentially_given_car() throws CustomException {
-        // given
-        Car car = new Car();
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        for (int i = 0; i < 2; i++) {
-            parkingLots.add(new ParkingLot());
-        }
-        NoSmartParkingBoy noSmartParkingBoy = new NoSmartParkingBoy(parkingLots);
-        // when
-        CarTicket ticket = noSmartParkingBoy.park(car);
-        // then
-        assertNotNull(ticket);
-        assertEquals(9, parkingLots.get(0).getParkingRoomRemindCapacity());
-        assertEquals(10, parkingLots.get(1).getParkingRoomRemindCapacity());
-    }
-
-    @Test
     void should_return_car_ticket_when_park_smartly_given_car() throws CustomException {
         // given
         Car car = new Car();
