4ac2f35b21798fae2f98aeca74875338eda2c443
HaloHan2017 <1006653740@qq.com>
2020-07-27 19:29:15 +0800

    [ParkingCar] [jerry] test : add should_get_exception_message_of_not_enough_position_when_park_given_car in NoSmartParkingBoyTest

--- src/test/java/com/oocl/cultivation/test/NoSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/NoSmartParkingBoyTest.java
@@ -27,4 +27,28 @@ class NoSmartParkingBoyTest {
         assertEquals(9, parkingLots.get(0).getParkingRoomRemindCapacity());
         assertEquals(10, parkingLots.get(1).getParkingRoomRemindCapacity());
     }
+
+    @Test
+    void should_get_exception_message_of_not_enough_position_when_park_given_car() {
+        // given
+        Car car = new Car();
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            parkingLots.add(new ParkingLot());
+        }
+        ParkingBoy noSmartParkingBoy = new NoSmartParkingBoy(parkingLots);
+        // when
+        String errorMsg = "";
+        try {
+            for (int i = 0; i < 10; i++) {
+                parkingLots.get(0).park(new Car());
+                parkingLots.get(1).park(new Car());
+            }
+            noSmartParkingBoy.park(car);
+        } catch (Exception e) {
+            errorMsg = e.getMessage();
+        }
+        // then
+        assertEquals("Not enough position.", errorMsg);
+    }
 }
