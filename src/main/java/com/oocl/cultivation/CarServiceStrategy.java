64bd9a300a642d406ee46da21d28c5dfbb592ebe
HaloHan2017 <1006653740@qq.com>
2020-07-27 19:50:27 +0800

    [ParkingCar] [jerry] refactor : remove try-catch

--- src/main/java/com/oocl/cultivation/CarServiceStrategy.java
+++ src/main/java/com/oocl/cultivation/CarServiceStrategy.java
@@ -2,17 +2,13 @@ package com.oocl.cultivation;
 
 public class CarServiceStrategy extends ParkingBoy implements IServiceStrategy {
     @Override
-    public Object doService(Object param) {
-        try {
-            Object result = null;
-            if (param instanceof Car) {
-                result = park((Car) param);
-            } else if (param instanceof CarTicket) {
-                result = fetch((CarTicket) param);
-            }
-            return result;
-        } catch (Exception e) {
-            return e.getMessage();
+    public Object doService(Object param) throws CustomException {
+        Object result = null;
+        if (param instanceof Car) {
+            result = park((Car) param);
+        } else if (param instanceof CarTicket) {
+            result = fetch((CarTicket) param);
         }
+        return result;
     }
 }
